695d8fd2898c1b98a43334097953189377dc5f32
Gotcha7770 <gbakanov@inbox.ru>
2022-07-30 23:52:01 +0300

    add tests

---
+++ Request.Validation.Tests/ResultTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Request.Validation.Tests;
+
+public class ResultTests
+{
+    [Fact]
+    public void Result_LINQ_Test()
+    {
+        var result1 = from x in new Result<int>(1)
+                      from y in new Result<int>(2)
+                      from z in new Result<int>(3)
+                      select x + y + z;
+
+        var output = result1.Match(x => x, _ => 0);
+        
+        var result2 = new Result<int>(1)
+            .SelectMany(x => new Result<int>(2), (x, y) => new { x, y })
+            .SelectMany(t => new Result<int>(3), (t, z) => t.x + t.y + z);
+    }
+    
+    [Fact]
+    public async Task AsyncResult_LINQ_Test()
+    {
+        var result1 = from x in AsyncResult.Ok(1)
+                      from y in AsyncResult.Ok(2)
+                      from z in AsyncResult.Ok(3)
+                      select x + y + z;
+
+        var output = await result1.Match(x => x, _ => 0);
+        
+        var result2 = AsyncResult.Ok(1)
+            .SelectMany(x => AsyncResult.Ok(2), (x, y) => new { x, y })
+            .SelectMany(t => AsyncResult.Ok(3), (t, z) => t.x + t.y + z);
+    }
+}
---
+++ Request.Validation.Tests/UnwrapTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Request.Validation.Tests;
+
+public class UnwrapTests
+{
+    private Task<Result<T>> Do<T>()
+    {
+        return Task.FromResult(new Result<T>(default(T)));
+    }
+    
+    [Fact]
+    public async Task UnwrapTest()
+    {
+        var result = Do<int>();
+        var result1 = await Do<int>();
+        var result2 = (await Do<int>()).Match(x => x * x, _ => -1);
+        var result4 = await Do<int>().Select(x => x.Match(r => r * r, _ => -1));
+    }
+    
+}
